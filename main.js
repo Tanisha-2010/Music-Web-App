16f03eb7f2db3b922b63765a59be553c9b571945
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-10-15 15:04:20 +0530

    Add files via upload
    C-128

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song_garmi = "";
 song_kesariya = "";
+left_wrist_x = 0;
+left_wrist_y = 0;
+right_wrist_x = 0;
+right_wrist_y = 0;
 
 function setup() {
     canvas = createCanvas(550, 430);
@@ -7,6 +11,27 @@ function setup() {
     ///canvas.center();
     video = createCapture(550, 430);
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded !!!");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        left_wrist_x = results[0].pose.leftWrist.x;
+        left_wrist_y = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + left_wrist_x + "Left Wrist Y = " + left_wrist_y);
+
+        right_wrist_x = results[0].pose.rightWrist.x;
+        right_wrist_y = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + right_wrist_x + "Right Wrist Y = " + right_wrist_y);
+    }
 }
 
 function preload() {
